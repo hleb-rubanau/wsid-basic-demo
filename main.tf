cd373486f167f33be65a8f76e6fd2e6ba149c4ec
Hleb Rubanau <g.rubanau@gmail.com>
2020-02-28 19:44:18 +0300

    wip

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
 provider "digitalocean" {
-  token = "${var.do_token}"
+  token = var.do_token
+}
+
+module "do_ssh_keys" {
+  source = "github.com/hleb-rubanau/terraform-module-digitalocean-ssh-key-ids"
+  keys   = var.keys
+}
+
+locals {
+  do_ssh_key_ids = values(module.do_ssh_keys.data)[*].id
+}
+
+output "result" {
+  value = local.do_ssh_key_ids
 }
--- variables.tf
+++ variables.tf
@@ -1,1 +1,5 @@
 variable "do_token" {}
+variable "keys" {
+  type    = list(string)
+  default = []
+}
